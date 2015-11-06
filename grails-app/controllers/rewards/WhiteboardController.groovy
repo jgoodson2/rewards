9c4c135e6e3e5b1537c6b1f331051cf0a36b94a5
Jonathon Goodson <goodson.jonathon@gmail.com>
2015-11-06 16:13:31 -0500

    Finished welcome action of CalculationsService.groovy

--- grails-app/controllers/rewards/WhiteboardController.groovy
+++ grails-app/controllers/rewards/WhiteboardController.groovy
@@ -1,11 +1,18 @@
 package rewards
 
 class WhiteboardController {
+    def calculationsService
 
     def index() {}
 
     def variables(){
         def myTotal = 1
+        render myTotal
 
+    }
+
+    def conditions(){
+        def welcomeMessage = calculationsService.welcome(params)
+        render welcomeMessage
     }
 }
--- grails-app/services/rewards/CalculationsService.groovy
+++ grails-app/services/rewards/CalculationsService.groovy
@@ -5,20 +5,24 @@ import grails.transaction.Transactional
 @Transactional
 class CalculationsService {
 
-    def welcome() {
+    def welcome(params) {
         def firstName = params.first
         def totalPoints = params.points.toInteger()
-        def welcomeMessage = ""
+        def welcomeMessage
 
         switch(totalPoints){
             case 5:
                 welcomeMessage = "Welcome back $firstName. This drink is on us."
+                break
             case 4:
                 welcomeMessage = "Welcome back $firstName. Your next drink is free."
+                break
             case 2..3:
                 welcomeMessage = "Welcome back $firstName. You now have $totalPoints points."
+                break
             default:
                 welcomeMessage = "Welcome $firstName. Thanks you for registering."
+                break
         }
     }
 }
